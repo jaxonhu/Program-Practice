da0464e443245fc5b265bdf67e0a3193bfae213f
hujiaxuan <hujiaxuan@bytedance.com>
2018-10-07 16:52:20 +0800

    scala

--- Scala/ScalaPractice/ChecksumAccumulator.scala
+++ Scala/ScalaPractice/ChecksumAccumulator.scala
@@ -1,6 +1,6 @@
 import scala.collection.mutable.Map
 
-<<<<<<< HEAD
+
 class ChecksumAccumulator {
 	
 	private var sum = 0
@@ -19,36 +19,11 @@ object ChecksumAccumulator {
 		else {
 			val acc = new ChecksumAccumulator
 			for(c <- s)
-=======
-object ChecksumAccumulator {
-	private val cache = Map[String, Int]()
-
-	def calculate(s : String): Int = 
-		if (cache.containes(s))
-			cache(s)
-		else {
-			val acc = new ChecksumAccumulator
-			for (c <- s)
->>>>>>> c45455c84dfdc7f2d82721d35386a15b58582927
 				acc.add(c.toByte)
 			val cs = acc.checksum()
 			cache += (s -> cs)
 			cs
-<<<<<<< HEAD
 		}	
 }
 
-val ca = new ChecksumAccumulator
-ca.add('3'.toByte)
-print(ca.checksum)
-=======
-		}
-}
 
-class ChecksumAccumulator {
-	private var sum = 0
-	def add(b : Byte) { sum += b }
-	def checksum(): Int = ~(sum & 0xff) + 1
-}
-
->>>>>>> c45455c84dfdc7f2d82721d35386a15b58582927
