04ad0722a190c35561f85ed2fd63af318f00c7a7
jaxonhu <hujiaxuan95@qq.com>
2018-12-14 14:43:02 +0800

    vue router demo

---
+++ vue/vue-router-test/index.vue
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div id="app">
+    <h1> hello world </h1>
+    <p>
+        <router-link to="/foo">Go To Foo</router-link>
+        <router-link to="/bar">Go To bar</router-link>
+    </p>
+    <router-view></router-view>
+</div>
+
+</body>
+</html>
+
+<script>
+    const Foo = { template: '<div>foo</div>'}
+    const Bar = {template: '<div>Bar</div>'}
+
+    const routes = [
+        {path: '/foo', component: Foo},
+        {path: '/bar', component: Bar}
+    ]
+
+    const router = new VueRouter({
+        routes
+    })
+
+    const app = new Vue({
+        router
+    }).$mount('#app')
+</script>
+
+
+
